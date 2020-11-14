813cef5a7c4417b6375262e94de172ba6fa39505
TronFlyn <ai974088741@outlook.com>
2020-11-14 19:08:33 +0800

    fixup do_cpu_updown for screen on/off

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -327,11 +327,13 @@ static int msm_drm_notifier_cb(struct notifier_block *nb, unsigned long action,
 		return NOTIFY_OK;
 
 	/* Boost when the screen turns on and unboost when it turns off */
-	if (*blank == MSM_DRM_BLANK_UNBLANK_CUST) {
+	if (*blank == MSM_DRM_BLANK_UNBLANK) {
 		clear_bit(SCREEN_OFF, &b->state);
+		do_cpu_updown(true);
 		__cpu_input_boost_kick_max(b, wake_boost_duration);
 	} else {
 		set_bit(SCREEN_OFF, &b->state);
+		do_cpu_updown(false);
 		wake_up(&b->boost_waitq);
 	}
 
